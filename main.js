d68643e4747e900b33ccd39be55c973653d34199
Valeriano <valeriano.tpc@gmail.com>
2020-08-24 23:23:56 -0300

    Classe in JS

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert('Teste')
+class TodoList{
+    constructor(){
+        this.todos =[];
+    }
+
+    addTodo(){
+        this.todos.push('Novo todo');
+        console.log(this.todos);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.addTodo();
+}
